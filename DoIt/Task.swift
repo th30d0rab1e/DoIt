4d75edab54bc397d969cae8ceff8c3557ee976e2
th30d0rab1e <darthtlc@gmail.com>
2018-04-08 12:26:38 -0500

    redoing project

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Theodore Cross on 2/5/18.
+//  Copyright © 2018 Theodore Cross. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -12,21 +12,43 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
         tableView.dataSource = self
         tableView.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello mate"
+        let task = tasks[indexPath.row]
+        
+        if task.important {
+            cell.textLabel?.text = "IMPORTANT! \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
         return cell
+    }
+    
+    func makeTasks() ->[Task]{
+        let task1 = Task()
+        task1.name = "walk the dog"
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        return [task1, task2]
     }
 }
 
